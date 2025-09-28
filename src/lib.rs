dda1d54e8c7c6ba4a63e0b8e2a5d1cecc60f697d
Dimitri <dimitri.timoz@orange.fr>
2025-09-28 21:01:25 +0200

    init Data trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub mod prelude;
+pub mod forcasting;
+
+pub trait Series {
+    fn as_slice(&self) -> &[f64];
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Series for Vec<f64> {
+    fn as_slice(&self) -> &[f64] {
+        self.as_slice()
     }
 }
