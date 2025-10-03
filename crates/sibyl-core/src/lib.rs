89c6382aeae199a00f28856db354d9b34444a5ef
Dimitri <dimitri.timoz@orange.fr>
2025-10-03 15:36:09 +0200

    feat: add TimeSeries and MultivariateSeries traits to enhance time series functionality

--- crates/sibyl-core/src/lib.rs
+++ crates/sibyl-core/src/lib.rs
@@ -8,3 +8,28 @@ pub trait Series {
     where
         Self: Sized;
 }
+
+pub trait TimeSeries: Series {
+    fn timestamps(&self) -> &[f64];
+}
+
+pub trait MultivariateSeries<S: Series> {
+    fn n_series(&self) -> usize;
+    fn series(&self, index: usize) -> Option<S>;
+}
+
+impl<T: Float> Series for Vec<T> {
+    type Elem = T;
+
+    fn as_slice(&self) -> &[Self::Elem] {
+        self.as_ref()
+    }
+
+    fn from(vec: Vec<Self::Elem>) -> Self
+    where
+        Self: Sized,
+    {
+        vec
+    }
+}
+
